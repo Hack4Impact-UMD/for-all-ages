2fabefd069547ae2a976ccf1b819f2ec667fbfdf
evant-ai <evan@airepublic.com>
2025-09-30 18:18:48 -0400

    removed template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <BrowserRouter>
