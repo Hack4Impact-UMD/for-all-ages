11b521dcbbbdd2234244af2df3ea9b1c43e563e5
evant-ai <evan@airepublic.com>
2025-10-20 10:01:23 -0400

    enlarged

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
+import Navbar from '../../components/Navbar'
 import styles from './Dashboard.module.css'
 
 export default function Dashboard () {
     return (
         <>
+            <Navbar></Navbar>
             <p className={styles.dashboardText}>Dashboard</p>
         </>
     )
