14b80755cf4bc93ca03f423f49dc82b175fdcdf5
Evan Tran <et2mbox@gmail.com>
2026-03-25 12:16:26 -0400

    fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -68,13 +68,6 @@ export const upsertUser = onRequest(async (req, res) => {
   try {
     const { uid, textResponses, numericResponses, user_type, pronouns } = req.body;
 
-    if (!uid || !freeResponse || !user_type) {
-      res.status(400).json({
-        error: "Missing required fields: uid, freeResponse, user_type",
-      });
-      return;
-    }
-
     // If there are no matchable responses, just return success
     if ((!textResponses || textResponses.length === 0) && 
         (!numericResponses || numericResponses.length === 0)) {
@@ -329,14 +322,14 @@ export const uploadProfilePicture = onRequest(async (req, res) => {
   let fileBuffer: Buffer | null = null;
   let fileMimeType = "image/jpeg";
 
-  bb.on("field", (name, value) => {
+  bb.on("field", (name: any, value: any) => {
     if (name === "uid") uid = value;
   });
 
-  bb.on("file", (_, file, info) => {
+  bb.on("file", (_: any, file: any, info: any) => {
     fileMimeType = info.mimeType;
     const chunks: Buffer[] = [];
-    file.on("data", (chunk) => chunks.push(chunk));
+    file.on("data", (chunk: any) => chunks.push(chunk));
     file.on("end", () => {
       fileBuffer = Buffer.concat(chunks);
     });
