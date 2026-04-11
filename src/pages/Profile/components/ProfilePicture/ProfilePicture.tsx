d2449860ef840fa75a1c0e2df02bad8b30273a8d
Hellen Ou <hou@umd.edu>
2026-04-11 10:41:13 -0400

    profile load fix

--- src/pages/Profile/components/ProfilePicture/ProfilePicture.tsx
+++ src/pages/Profile/components/ProfilePicture/ProfilePicture.tsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../../Profile.module.css";
 
 const ProfilePicture = ({
   uid,
   size = 64,
+  version,
 }: {
   uid?: string;
   size?: number;
+  version?: number;
 }) => {
   const [imageError, setImageError] = useState(false);
+
+  useEffect(() => {
+    setImageError(false);
+  }, [uid, version]);
 
   const defaultSrc =
     "https://i.pinimg.com/474x/33/f8/26/33f8266681c946cd80de486c499fe992.jpg";
 
   const src = uid
-    ? `https://storage.googleapis.com/for-all-ages-cdn/profile-pictures/${uid}?t=${Date.now()}`
+    ? `https://storage.googleapis.com/for-all-ages-cdn/profile-pictures/${uid}${version !== undefined ? `?t=${version}` : ""}`
     : "";
 
   return (
